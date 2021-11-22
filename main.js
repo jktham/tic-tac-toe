59b7f3d04abb6cb9c22895f888967b44c4bf1062
jktham-pc <jonas.tham@gmx.ch>
2021-11-22 18:23:22 +0100

    Fix click behavior, toggle players after action

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ const game = (() => {
     const player2_name = document.querySelector("#player2-name");
     const player2_symbol = document.querySelector("#player2-symbol");
     
-        
+    let player;
+    let player1;
+    let player2;
+    let board_state;
+
     const Player = (name, symbol, index) => {
         let points = 0;
         const getName = () => name;
@@ -30,15 +34,23 @@ const game = (() => {
 
     const startGame = () => {
         if (player1_name.value && player1_symbol.value && player2_name.value && player2_symbol.value) {
-            let player1 = Player(player1_name.value, player1_symbol.value, 1);
-            let player2 = Player(player2_name.value, player2_symbol.value, 2);
-            let player = player1;
+            player1 = Player(player1_name.value, player1_symbol.value, 1);
+            player2 = Player(player2_name.value, player2_symbol.value, 2);
+            togglePlayer();
             createBoard();
         }
     }
 
+    const togglePlayer = () => {
+        if (player != player1) {
+            player = player1;
+        } else {
+            player = player2;
+        }
+    }
+
     const createBoard = () => {
-        let board_state = Array.from(Array(3), () => new Array(3));
+        board_state = Array.from(Array(3), () => new Array(3));
         setup.style.display = "none";
         let board = document.createElement("div");
         board.classList.add("board");
@@ -70,6 +82,7 @@ const game = (() => {
     const clickedCell = (e) => {
         board_state[e.target.dataset.y][e.target.dataset.x] = player.getIndex();
         e.target.innerHTML = player.getSymbol();
+        togglePlayer();
     }
     
     start_button.addEventListener("click", startGame);
